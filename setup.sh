19576aae2b100df9b3d7327ae86ec00251db23f3
Biraru <muhammadbilalalisyed@gmail.com>
2025-09-14 10:28:45 +0500

    feat(traefik): use traefik subdomain for traefik

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 CERTS_DIR="$SCRIPT_DIR/docker/certificates"
 CODE_DIR="$SCRIPT_DIR/code"
 
+# Clone repos
 for REPO in "panel" "documentation" "wings"
 do
   if [ ! -d "$CODE_DIR/$REPO" ]; then
@@ -12,23 +14,25 @@ do
   fi
 done
 
+# Install mkcert and create certs for services
 mkcert -install
-mkcert pterodactyl.test wings.pterodactyl.test minio.pterodactyl.test s3.minio.pterodactyl.test
+mkcert pterodactyl.test traefik.pterodactyl.test wings.pterodactyl.test minio.pterodactyl.test s3.minio.pterodactyl.test
 
-# Because we're doing Docker-in-Docker we actually need these paths to line
-# up correctly with the host system.
+# Because we're doing Docker-in-Docker we actually need these paths to line up correctly with the host system.
 sudo mkdir -p /var/lib/pterodactyl
 sudo chown $(id -u):$(id -g) /var/lib/pterodactyl
 
+# Move all certs
 mv -v *pterodactyl.test*-key.pem docker/certificates/pterodactyl.test-key.pem || exit 1
 mv -v *pterodactyl.test*.pem docker/certificates/pterodactyl.test.pem || exit 1
 cp -v "$(mkcert -CAROOT)/rootCA.pem" docker/certificates/root_ca.pem || exit 1
 
+# Setup host file
 echo ""
 if [ ! -f "/etc/hosts" ]; then
   echo "no system hosts file found, please manually configure your system"
 else
-  for DOMAIN in "pterodactyl.test" "wings.pterodactyl.test" "minio.pterodactyl.test" "s3.minio.pterodactyl.test"
+  for DOMAIN in "pterodactyl.test" "traefik.pterodactyl.test" "wings.pterodactyl.test" "minio.pterodactyl.test" "s3.minio.pterodactyl.test"
   do
     ESCAPED_DOMAIN=$(echo $DOMAIN | sed "s/\./\\\./g")
     if ! grep -q -E "127\.0\.0\.1\s+$ESCAPED_DOMAIN\s*$" /etc/hosts; then
@@ -40,6 +44,7 @@ else
   done
 fi
 
+# Optional beak alias
 echo "optionally, configure the beak alias:"
 
 echo "bash:"
